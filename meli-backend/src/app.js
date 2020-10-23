5d215d3724fb3aeed1d3ba7e5fac8082866323da
Gonzalo Vanni <gonzalovanni@gmail.com>
2020-10-23 00:31:32 -0300

    Update app.js
    cors middleware

--- meli-backend/src/app.js
+++ meli-backend/src/app.js
@@ -4,8 +4,10 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const apiRouter = require('./routes/api/apiRoutes')
 const authorMiddleware = require('./middlewares/api/author')
+const cors = require('cors')
 const app = express();
 
+app.use(cors())
 app.use(authorMiddleware)
 app.use(logger('dev'));
 app.use(express.json());
